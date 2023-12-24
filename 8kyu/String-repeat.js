49e598d2a83bf19d1204cb52f21a5ac6e97ae1f9
Ihnatsy <ihpo5550@gmail.com>
2023-12-24 13:24:19 +0300

    add emoji in String repeat

--- 8kyu/String-repeat.js
+++ 8kyu/String-repeat.js
@@ -1,11 +1,11 @@
-// DESCRIPTION:
+// ❓ DESCRIPTION:
 //     Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 //
 // Examples (input -> output)
 // 6, "I"     -> "IIIIII"
 // 5, "Hello" -> "HelloHelloHelloHelloHello"
 
-//  SOLUTION:
+//  🥳 SOLUTION:
 function repeatStr (n, s) {
     let string=''
     for (let i=1; i<=n; i++) {
