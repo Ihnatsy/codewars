37d32f2bc10d1b5502488e7fda5356b770f02837
Ihnatsy <ihpo5550@gmail.com>
2023-12-24 13:17:42 +0300

    Find the smallest integer in the array

---
+++ 8kyu/Remove-First-and-Last-Character.js
@@ -0,0 +1,13 @@
+// ❓ DESCRIPTION:
+//     It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+//     You're given one parameter, the original string.
+//     You don't have to worry with strings with less than two characters.
+
+// 🥳 SOLUTION:
+let str ='You got this!';
+
+function removeChar(str){
+
+    str= str.substring(1,str.length-1);
+    return str;
+}
