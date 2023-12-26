d9e6d4714b0ae183434daaf04d785af09e4dded7
Ihnatsy <ihpo5550@gmail.com>
2023-12-26 16:29:13 +0300

    add emoji in Convert a Boolean to a String

--- 8kyu/convert-a-Boolean-to-a-String.js
+++ 8kyu/convert-a-Boolean-to-a-String.js
@@ -1,9 +1,9 @@
-// DESCRIPTION:
+// ❓ DESCRIPTION:
 //     Implement a function which convert the given boolean value into its string representation.
 //
 //     Note: Only valid inputs will be given.
 
-// SOLUTION:
+// 🥳 SOLUTION:
 function booleanToString(b){
     return String(b)
 }
